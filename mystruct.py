8ac2eac75a31f51370d9bad18c59cb7ef718e699
shinsat <kiaorasat@gmail.com>
2017-05-17 23:02:27 +0900

    辞書パラメータ渡し検証

---
+++ mystruct.py
@@ -0,0 +1,13 @@
+# http://hiro-itasuto7.hatenadiary.jp/entry/2011/03/23/184607
+# http://www.yoheim.net/blog.php?q=20160610
+DATA = {'ID1':'10', 'ID2':'20', 'ID3':['30','100','200']}
+
+class mine:
+    def __init__(self, **info):
+        self.myinfo = info
+        print(self.myinfo)
+
+        for param in info.keys():
+            print('key:', param, 'value:', info[param])
+
+a = mine(**DATA)
