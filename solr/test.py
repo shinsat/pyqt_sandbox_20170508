631f2da7cff73b14ed2778363fd9297d07d4e21c
shinsat <kiaorasat@gmail.com>
2017-05-18 00:02:45 +0900

    solrのデルタ更新

--- solr/test.py
+++ solr/test.py
@@ -1,11 +1,12 @@
 #
 #  https://pypi.python.org/pypi/pysolr/3.5.0#downloads
-#
+#  http://stackoverflow.com/questions/24415659/how-to-make-atomic-updates-to-solr-using-pysolr
 
 import sys
 import pysolr
 
 solr = pysolr.Solr('http://localhost:8983/solr/', timeout=10)
+# add
 solr.add([
 {
         "id": "doc_1",
@@ -17,10 +18,15 @@ solr.add([
     },
 ])
 
-
+# search
 results = solr.search('book')
 #print("Saw {0} result(s).".format(len(results)))
 for result in results:
     print("The title is '{0}'.".format(result['id']))
 
+# delete
 solr.delete(id='doc_2')
+
+# partial update
+doc = {'id':'978-1423103349', 'genre_s':'Science Fantasy'}
+solr.add([doc], fieldUpdates={'genre_s':'set'})
